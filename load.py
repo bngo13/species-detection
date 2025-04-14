e9c9013b826b7b7a486a29bb0972e2473a603959
bngo13 <b_ngo13@u.pacific.edu>
2025-04-14 12:21:37 -0700

    Updated loading

--- load.py
+++ load.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 import pandas as pd
+import numpy as np
 
 import cv2
 
-DIR_LOCATION = "PROJDATASET"
+DIR_LOCATION = "/dev/shm/PROJDATASET"
 ID_LIST = pd.read_csv(f"{DIR_LOCATION}/ostracod_IDs.csv")
 
 def load_dataset():
@@ -14,18 +15,27 @@ def load_dataset():
     # Load Image Data to DataFrame
     for _, row in ID_LIST.iterrows():
         print(f"Reading Index: {row['OrganismID']}")
+
         # Load the image into the row
         dir_path = list(Path(DIR_LOCATION).glob(f"{row['OrganismID']:03}*"))[0]
-        file_paths = Path(dir_path).glob("*.png")
+        file_paths = Path(dir_path).glob("*.tif")
         for file in file_paths:
-            row_data = [row["Species"], row["Sex"], row["Stage"], row["location"]]
-            resized_img = cv2.resize(cv2.imread(file, cv2.IMREAD_GRAYSCALE), (1500, 1000)).flatten()
-            row_data.extend(resized_img)
-            data_df = pd.concat([data_df, pd.DataFrame(row_data).T])
+            headers = np.array([row["Species"], row["Sex"], row["Stage"], row["location"]])
+            resized_img = np.array(cv2.resize(cv2.imread(file, cv2.IMREAD_GRAYSCALE), (1500, 1000)).flatten()).astype(np.uint8)
+            header_df = pd.DataFrame(headers).T
+            header_df[2] = header_df[2].astype(np.uint8)
+            img_df = pd.DataFrame(resized_img).T
+            row_df = pd.concat([header_df, img_df], axis=1)
+            data_df = pd.concat([data_df, row_df])
+    data_df = data_df.reset_index()
+    data_df = data_df.drop("index", axis=1)
+    data_df = data_df.dropna()
+    
+    # Rename columns
+    data_df.columns.values[0] = "species"
+    data_df.columns.values[1] = "sex"
+    data_df.columns.values[2] = "stage"
+    data_df.columns.values[3] = "location"
 
-    data_df.reset_index()
-    data_df.drop("index", axis=1)
-    data_df.dropna()
-    data_df.to_csv("/dev/shm/bngo.csv", index=False)
+    return data_df
 
-load_dataset()
---
+++ train.py
@@ -0,0 +1,8 @@
+import load
+
+def main():
+    dataset = load.load_dataset()
+    breakpoint()
+
+if __name__ == "__main__":
+    main()
