92a8f9764b610f0f42704e42719e410b2ee27786
Brian Ngo <b_ngo13@u.pacific.edu>
2025-04-23 12:12:11 -0700

    Fix

--- tensorFiles/tensorTrain.py
+++ tensorFiles/tensorTrain.py
@@ -137,7 +137,7 @@ def run_kfold_training(layer1, layer2):
     hist = model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS, callbacks=[checkpoint_callback, early_stopping_callback])
     acc = hist.history['accuracy'][-1]
     val_acc = hist.history['val_accuracy'][-1]
-    model.save(f"{OUTPUT_LOCATION}/model_fold{fold}_acc{acc}_vacc{val_acc:.4f}")
+    model.save(f"{OUTPUT_LOCATION}/model_fold{fold}_acc{acc}_vacc{val_acc:.4f}.keras")
 
 
 # Run it
